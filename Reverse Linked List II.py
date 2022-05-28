8060ba20f96df084e8e24de7d958caf0a1c1f3fb
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-05-27 17:51:54 -0700

    Add files via upload

---
+++ Reverse Linked List II.py
@@ -0,0 +1,26 @@
+# 92. Reverse Linked List II
+
+# Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        if left == right:
+            return head
+        p = dummy = ListNode(0)
+        dummy.next = head
+        for _ in range(left - 1):
+            p = p.next
+        cur = p.next
+        pre = None
+        for _ in range(right - left + 1):
+            cur.next, pre, cur = pre, cur, cur.next
+        p.next.next = cur
+        p.next = pre
+        return dummy.next
+        
+        return dummyNode.next
